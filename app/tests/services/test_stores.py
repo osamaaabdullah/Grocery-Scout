3912055b4995909b99d65506cc61639b6c8a4bff
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2025-09-26 22:40:04 -0400

    test: complete unit tests for Store services

--- app/tests/services/test_stores.py
+++ app/tests/services/test_stores.py
@@ -83,3 +83,104 @@ def test_get_store_by_id_returns_store():
     assert result == fake_store
     mock_db.query.assert_called_once_with(Store)
 
+def test_get_stores_retuns_list():
+    fake_stores = [
+        Store(
+        store_name = "Test Store 01",
+        store_id = 1234,
+        city = "Test City",
+        postal_code =  "TES T12",
+        retailer = "Test",
+        store_province = "TS",
+        longitude =  -12.3456789,
+        latitude = 98.7654321,),
+        Store(
+        store_name = "Test Store 02",
+        store_id = 4321,
+        city = "Test City",
+        postal_code =  "TES T34",
+        retailer = "Test",
+        store_province = "TS",
+        longitude =  -12.3456789,
+        latitude = 98.7654321,)
+    ]
+
+    mock_db = MagicMock()
+    mock_db.query.return_value.all.return_value = fake_stores
+
+    results = stores.get_stores(mock_db)
+
+    assert results == fake_stores
+    mock_db.query.assert_called_once_with(Store)
+    mock_db.query.return_value.all.assert_called_once()
+
+def test_get_stores_by_province_returns_list():
+    fake_stores = [
+        Store(
+        store_name = "Test Store 01",
+        store_id = 1234,
+        city = "Test City",
+        postal_code =  "TES T12",
+        retailer = "Test",
+        store_province = "TS",
+        longitude =  -12.3456789,
+        latitude = 98.7654321,),
+        Store(
+        store_name = "Test Store 02",
+        store_id = 4321,
+        city = "Test City",
+        postal_code =  "TES T34",
+        retailer = "Test",
+        store_province = "TS",
+        longitude =  -12.3456789,
+        latitude = 98.7654321,)
+    ]
+
+    mock_db = MagicMock()
+    mock_db.query.return_value.filter.return_value.all.return_value = fake_stores
+
+    result = stores.get_stores_by_province(mock_db, "TS")
+
+    assert result == fake_stores
+    mock_db.query.assert_called_once_with(Store)
+    mock_db.query.return_value.filter.return_value.all.assert_called_once()
+
+def test_haversine_zero_distance():
+    distance = stores.haversine(49.2657132,-123.2564381,49.2657132,-123.2564381)
+    assert distance == 0
+
+def test_haversine_known_distance():
+    distance = stores.haversine(49.2657132,-123.2564381,49.2281003,-123.1997837)
+    assert 5 < distance < 8
+
+def test_get_nearest_stores_returns_within_distance_list():
+    fake_stores = [
+        Store(
+        store_name = "Test Store 01",
+        store_id = 1234,
+        city = "Test City",
+        postal_code =  "TES T12",
+        retailer = "Test",
+        store_province = "TS",
+        longitude =  -40,
+        latitude = 90,),
+        Store(
+        store_name = "Test Store 02",
+        store_id = 4321,
+        city = "Test City",
+        postal_code =  "TES T34",
+        retailer = "Test",
+        store_province = "TS",
+        longitude =  -10,
+        latitude = 10,)
+    ]
+
+    mock_db = MagicMock()
+    mock_db.query.return_value.filter.return_value.all.return_value = fake_stores
+
+    results = stores.get_nearest_stores(mock_db, user_lat= 89, user_lon=-30, set_distance=150)
+
+    assert len(results) == 1
+    assert results[0]["store_name"] == "Test Store 01"
+    mock_db.query.assert_called_once_with(Store)
+    mock_db.query.return_value.filter.return_value.all.assert_called_once()
