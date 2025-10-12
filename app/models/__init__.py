8446f5d3f69292c0a0c3e24858f756810f6bca5e
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2025-10-11 21:44:09 -0400

    generate alembic script for users and oauth_account tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.database import Base
 
 from .store_product import Store, Product, Price, PriceHistory
-from .user import User, OAuthAccount, RefreshToken
+from .user import User, OAuthAccount
 from .metro_cookie import MetroChainCookie
 from .province_price import ProvincePrice
