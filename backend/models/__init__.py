496f4b3ed79438794413a51b398cd944817eb816
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2025-11-25 11:32:04 -0500

    drop metro cookie table

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,5 +2,4 @@ from backend.database import Base
 
 from .store_product import Store, Product, Price, PriceHistory
 from .user import User, OAuthAccount
-from .metro_cookie import MetroChainCookie
 from .province_price import ProvincePrice
