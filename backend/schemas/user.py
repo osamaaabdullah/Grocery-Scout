e10bad2f3b9e80315ca7c73f734afd0ced8c17d8
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-03-23 23:13:54 -0400

    fix(schemas/user): clean up user schemas

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 class UserBase(BaseModel):
     email: EmailStr
-    is_verified: bool
     provider: Optional[str] = None
 
 class UserCreate(BaseModel):
@@ -19,9 +18,11 @@ class UserLogin(BaseModel):
 
 class UserOut(UserBase):
     user_id: int
+    name: str
+    role: str
     created_at: datetime
     updated_at: datetime
-    
+    is_verified: bool
     model_config = ConfigDict(from_attributes = True)
         
 
@@ -42,9 +43,6 @@ class OAuthAccount(OAuthAccountBase):
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-class TokenData(BaseModel):
-    email: EmailStr | None = None
 
 class PasswordResetEmail(BaseModel):
     email: EmailStr
