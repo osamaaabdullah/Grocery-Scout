7b31cc8dce425d9f141acec551e3e92f36790459
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-03-23 23:18:53 -0400

    refactor(routers/province_prices): fix imports and error handling

--- backend/routers/province_prices.py
+++ backend/routers/province_prices.py
@@ -1,17 +1,16 @@
 import backend.services.province_prices as province_price_services
-import backend.services.products as product_services
 import backend.services.stores as store_services
 import backend.services.geocode as geocode_services
 
 from fastapi import APIRouter
 from backend.schemas.province_price import ProvincePriceCreate
-from typing import List
 from sqlalchemy.orm import Session
 from fastapi import Depends
-from backend.database import get_read_db, get_write_db
-from backend.models import User
+from backend.dependencies.db import get_read_db, get_write_db
+from backend.models.user import User
 from typing import Annotated
-from backend.services.auth import role_required
+from backend.dependencies.auth import role_required
+from backend.core.exceptions import InvalidPostalCodeError
 
 router = APIRouter(prefix="/province", tags = ["Province prices"])
 
@@ -20,7 +19,7 @@ async def upsert_price(price: ProvincePriceCreate, db: Session = Depends(get_wri
     return province_price_services.upsert_price(db, price)
 
 @router.post("/prices")
-async def upsert_prices(prices: List[ProvincePriceCreate], db: Session = Depends(get_write_db), current_user: Annotated[User, Depends(role_required("admin"))] = None):
+async def upsert_prices(prices: list[ProvincePriceCreate], db: Session = Depends(get_write_db), current_user: Annotated[User, Depends(role_required("admin"))] = None):
     return province_price_services.upsert_prices(db,prices)
      
 @router.get("/price/{product_id}")
@@ -36,8 +35,10 @@ async def search_price_by_product(product_name: str, category: str = None, multi
     return province_price_services.get_product_and_price(db, product_name, category, multi_offer=multi_offer)
 
 @router.get("/prices/search-nearby")
-def search_nearby_products(product_name: str, postal_code: str, set_distance: float = 5, category: str = None, db: Session = Depends(get_read_db)):
+async def search_nearby_products(product_name: str, postal_code: str, set_distance: float = 5, category: str = None, db: Session = Depends(get_read_db)):
     user_geo = geocode_services.get_geocode_from_postal(postal_code)
+    if "message" in user_geo:
+        raise InvalidPostalCodeError()
     nearest = store_services.get_nearest_stores(db, user_geo["lat"], user_geo["lng"], set_distance)
     province = nearest[0]["store_province"] if nearest else None
     return province_price_services.get_product_and_price(db, product_name, province=province, nearest_stores=nearest, category=category)
