f2c3809d893e0a8ffe19b759f794cc13bbeb8201
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-03-27 22:05:34 -0400

    backend refactor (router/province_prices): remove search and search-nearby endpoints

--- backend/routers/province_prices.py
+++ backend/routers/province_prices.py
@@ -30,20 +30,6 @@ async def get_product_price(product_id: str, retailer: str = None, db: Session =
 async def get_all_products_and_prices(category: str = None, retailer: str = None, province: str = "ON", postal_code: str = None, page: int = 1, sort_by: str = None, sort_order: str = None, multi_offer: bool = False, db: Session = Depends(get_read_db)):
     return province_price_services.get_all_products_and_prices(db, category, retailer, postal_code=postal_code, province=province, page = page, limit = 20, sort_by=sort_by, sort_order=sort_order, multi_offer=multi_offer)
 
-@router.get("/prices/search")
-async def search_price_by_product(product_name: str, category: str = None, multi_offer: bool = False, db: Session = Depends(get_read_db), page: int = 1):
-    return province_price_services.get_product_and_price(db, product_name, category, multi_offer=multi_offer, page=page)
-
-@router.get("/prices/search-nearby")
-async def search_nearby_products(product_name: str, postal_code: str, set_distance: float = 5, category: str = None, db: Session = Depends(get_read_db), page: int = 1):
-    user_geo = geocode_services.get_geocode_from_postal(postal_code)
-    if "message" in user_geo:
-        raise InvalidPostalCodeError()
-    nearest = store_services.get_nearest_stores(db, user_geo["lat"], user_geo["lng"], set_distance)
-    province = nearest[0]["store_province"] if nearest else None
-    return province_price_services.get_product_and_price(db, product_name, province=province, nearest_stores=nearest, category=category, page=page)
-
-
 @router.delete("/price/{product_id}")
 async def delete_price(product_id: str, db: Session = Depends(get_write_db), current_user: Annotated[User, Depends(role_required("admin"))] = None):
     return province_price_services.delete_price(db,product_id)
