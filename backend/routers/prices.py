5dbea802a4034764e41a7713c10bbbf7ad13c0c1
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-03-27 22:07:11 -0400

    backend refactor (routers/prices): remove search-nearby endpoint, update search endpoint to fetch live prices

--- backend/routers/prices.py
+++ backend/routers/prices.py
@@ -11,6 +11,7 @@ from backend.models.user import User
 from typing import Annotated
 from backend.dependencies.auth import role_required
 from backend.core.exceptions import InvalidPostalCodeError
+from backend.services.search import search_products_with_live_prices
 
 router = APIRouter(tags=["Individual Store Prices"])
 
@@ -23,16 +24,12 @@ async def upsert_prices(prices: list[PriceCreate], db: Session = Depends(get_wri
     return price_services.upsert_prices(db,prices) 
 
 @router.get("/prices/search")
-async def search_price_by_product(product_name: str, category: str = None, multi_offer: bool = False, page: int = 1, db: Session = Depends(get_read_db)):
-    return price_services.get_product_and_price(db, product_name = product_name, category=category, multi_offer=multi_offer, page=page)
-
-@router.get("/prices/search-nearby")
 async def search_nearby_products(product_name: str, postal_code: str, set_distance: float = 5, category: str = None, multi_offer: bool = False, page: int = 1, db: Session = Depends(get_read_db)):
     user_geo = geocode_services.get_geocode_from_postal(postal_code)
     if "message" in user_geo:
         raise InvalidPostalCodeError()
     nearest = store_services.get_nearest_stores(db, user_geo["lat"], user_geo["lng"], set_distance)
-    return price_services.get_product_and_price(db, product_name, category=category, nearest_stores= nearest, multi_offer= multi_offer, page=page)
+    return search_products_with_live_prices(db, product_name, category=category, nearest_stores= nearest, multi_offer= multi_offer, page=page)
 
 @router.get("/prices")
 async def get_all_products_and_prices(category: str = None, retailer: str = None, db: Session = Depends(get_read_db)):
