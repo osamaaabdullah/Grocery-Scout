d2b2ac2894172ba50c72463f3c60162166354545
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-03-28 23:09:20 -0400

    frontend refactor: remove props from SearchBar

--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -3,43 +3,35 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { SearchIcon } from 'lucide-react';
+import { useLocation } from "@/context/LocationContext";
 
-interface SearchBarProps {
-  postalCode?: string;
-  distance?: string;
-}
+export default function SearchBar() {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+  const { postalCode, distance } = useLocation();
 
-export default function SearchBar({ postalCode, distance}: SearchBarProps) {
-    const [query, setQuery] = useState("");
-    const router = useRouter();
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!query) return;
+    router.push(`/results?search=${encodeURIComponent(query)}&postal_code=${postalCode}&set_distance=${distance}`);
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (!query) return;
-        router.push(`/results?search=${encodeURIComponent(query)}&postal_code=${postalCode}&set_distance=${distance}`);
-    };
-
-    return (
-        <form onSubmit={handleSubmit} className="flex gap-2 items-center " >
-                <div className="pl-1 flex-grow">
-                    <input
-                    type="text"
-                    placeholder="Search products"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                    className="pl-3 w-full rounded-full"
-                    />
-                </div>
-                
-                <div>
-                    <button
-                    type="submit"
-                    className="flex pr-3"
-                    >
-                    <SearchIcon/>
-                    </button>
-                </div>
-        </form>
-
-    );
+  return (
+    <form onSubmit={handleSubmit} className="flex gap-2 items-center">
+      <div className="pl-1 flex-grow">
+        <input
+          type="text"
+          placeholder="Search products"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="pl-3 w-full rounded-full"
+        />
+      </div>
+      <div>
+        <button type="submit" className="flex pr-3">
+          <SearchIcon />
+        </button>
+      </div>
+    </form>
+  );
 }
